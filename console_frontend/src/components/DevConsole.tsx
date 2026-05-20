93c58a7d9f226f7edc6de87e3b0b4a2ecd13692f
cree1116 <leejongmin774@gmail.com>
2026-05-20 19:58:31 +0900

    수정

--- console_frontend/src/components/DevConsole.tsx
+++ console_frontend/src/components/DevConsole.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { 
-  BarChart2, Play, RefreshCw, Upload, 
-  CheckCircle2, AlertCircle, Info, X
+  BarChart2, Play, RefreshCw, Upload,
+  CheckCircle2, AlertCircle, Info, X, Wand2
 } from 'lucide-react';
 import client from '../api/client';
 
